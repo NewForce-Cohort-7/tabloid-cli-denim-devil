9381325bf483aa2ab6b7c2870b5517fb172627aa
John Sheets <johnsheets410@gmail.com>
2023-06-21 13:45:19 -0400

    first-three-tickets

--- TabloidCLI/UserInterfaceManagers/TagManager.cs
+++ TabloidCLI/UserInterfaceManagers/TagManager.cs
@@ -69,10 +69,52 @@ namespace TabloidCLI.UserInterfaceManagers
             tag.Name = Console.ReadLine();
             _tagRepository.Insert(tag);
         }
+        private Tag Choose(string prompt = null)
+        {
+            if (prompt == null)
+            {
+                prompt = "Choose a tag to edit";
+            }
 
+            Console.WriteLine(prompt);
+
+            List<Tag> tags = _tagRepository.GetAll();
+
+            for (int i = 0; i < tags.Count; i++)
+            {
+                Tag tag = tags[i];
+                Console.WriteLine($" {i + 1}) {tag.Name}");
+            }
+            Console.Write("> ");
+
+            string input = Console.ReadLine();
+            try
+            {
+                int choice = int.Parse(input);
+                return tags[choice - 1];
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Invalid Selection");
+                return null;
+            }
+        }
         private void Edit()
         {
-            throw new NotImplementedException();
+            Tag tagToEdit = Choose("Which tag would you like to edit?");
+            if (tagToEdit == null)
+            {
+                return;
+            }
+            Console.WriteLine();
+            Console.Write("New tag name (blank to leave unchanged: ");
+            string Name = Console.ReadLine();
+            if (!string.IsNullOrWhiteSpace(Name))
+            {
+                tagToEdit.Name = Name;
+            }
+            _tagRepository.Update(tagToEdit);
+
         }
 
         private void Remove()
